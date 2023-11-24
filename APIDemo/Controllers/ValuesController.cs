57302f11937b33e015d706689654c5e9ab6394e3
deoresulakshana <deoresulakshana@gmail.com>
2023-11-24 15:57:15 +0530

    Add project files.

---
+++ APIDemo/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIDemo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        string[] arr = new string[3] { "Sourav", "Gourav", "" };
+       
+        // GET api/values
+        public IEnumerable<string> Get()//ShowAll
+        {
+            return arr;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            string v = null;
+            if (id==0)
+            {
+                v = "Sourav";
+            }
+            else
+            {
+                v = "Gourav";
+            }
+            return v;
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)//Insert
+        {
+            arr[2]=value;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+            string v = null;
+            if (id == 0)
+            {
+                v = value;
+            }
+            else
+            {
+                v = value;
+            }
+            
+
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            if (id == 0)
+            {
+                arr[id] = null;
+            }
+            else
+            {
+                arr[id] = null;
+            }
+            
+        }
+    }
+}
